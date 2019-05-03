7b711908de0ec1fa6c14166c2c13318b0993fa40
Nathaniel McCallum <npmccallum@redhat.com>
2019-05-03 13:41:10 -0400

    Add Cr0Flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,34 @@
 use bitflags::bitflags;
+
+bitflags! {
+    /// Configuration flags of the Cr0 register.
+    pub struct Cr0Flags: u64 {
+        /// Enables protected mode.
+        const PROTECTED_MODE_ENABLE = 1 << 0;
+        /// Enables monitoring of the coprocessor, typical for x87 instructions.
+        ///
+        /// Controls together with the `TASK_SWITCHED` flag whether a `wait` or `fwait`
+        /// instruction should cause a device-not-available exception.
+        const MONITOR_COPROCESSOR = 1 << 1;
+        /// Force all x87 and MMX instructions to cause an exception.
+        const EMULATE_COPROCESSOR = 1 << 2;
+        /// Automatically set to 1 on _hardware_ task switch.
+        ///
+        /// This flags allows lazily saving x87/MMX/SSE instructions on hardware context switches.
+        const TASK_SWITCHED = 1 << 3;
+        /// Enables the native error reporting mechanism for x87 FPU errors.
+        const NUMERIC_ERROR = 1 << 5;
+        /// Controls whether supervisor-level writes to read-only pages are inhibited.
+        ///
+        /// When set, it is not possible to write to read-only pages from ring 0.
+        const WRITE_PROTECT = 1 << 16;
+        /// Enables automatic alignment checking.
+        const ALIGNMENT_MASK = 1 << 18;
+        /// Ignored. Used to control write-back/write-through cache strategy on older CPUs.
+        const NOT_WRITE_THROUGH = 1 << 29;
+        /// Disables internal caches (only for some cases).
+        const CACHE_DISABLE = 1 << 30;
+        /// Enables page translation.
+        const PAGING = 1 << 31;
+    }
+}
